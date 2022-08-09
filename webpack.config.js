10ec61a2ae9751a5e0582b4c019789749dc65186
Matias Aguirre <aguirremati09@gmail.com>
2022-08-09 12:15:48 -0300

    Change mode to production

--- webpack.config.js
+++ webpack.config.js
@@ -2,12 +2,13 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+mode: 'production',  
   entry: './src/index.js',
   devServer: {
     static: './dist',
   },
   output: {
-    filename: 'main.js',
+    filename: '[name].js',
     path: path.resolve(__dirname, 'dist'),
     clean: true,
   },
